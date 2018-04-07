77008c3b123f43e04763c4768ca4270ca68fcac6
Unknown <essalguero@yahoo.es>
2018-04-07 02:50:48 +0200

    Included Undo/Redo of last deleted action

--- Test01/mainwindow.cpp
+++ Test01/mainwindow.cpp
@@ -1,13 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    QObject::connect(ui->pButtonAdd,SIGNAL(clicked()), this, SLOT(on_push_pButtonAdd()));
+    QObject::connect(ui->pButtonAdd,SIGNAL(clicked()), this, SLOT(onItemAdded()));
 
     QObject::connect(ui->pButtonDelete,SIGNAL(clicked()), this, SLOT(on_push_pButtonDelete()));
 
@@ -18,6 +20,34 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QObject::connect(ui->actionQuit,SIGNAL(triggered(bool)), this, SLOT(on_triggered_Quit()));
 
+    QObject::connect(ui->actionUndo,SIGNAL(triggered(bool)), this, SLOT(on_triggered_Undo()));
+
+    QObject::connect(ui->actionRedo,SIGNAL(triggered(bool)), this, SLOT(on_triggered_Redo()));
+
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* event)
+{
+    if (focusWidget() == ui->lWidgetTareas)
+    {
+        if (event->key() == Qt::Key_Delete || event->key() == Qt::Key_Backspace)
+        {
+            on_push_pButtonDelete();
+        }
+        else if (event->key() == Qt::Key_Return)
+        {
+            on_push_pButtonEdit();
+        }
+        else if (event->key() == Qt::Key_Enter)
+        {
+            on_push_pButtonComplete();
+        }
+    }
+    else if (focusWidget() == ui->lEditTarea)
+    {
+        if (event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return)
+            onItemAdded();
+    }
 }
 
 MainWindow::~MainWindow()
@@ -26,12 +56,28 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_push_pButtonAdd()
+void MainWindow::onItemAdded()
 {
     if (ui->lEditTarea->text() != "")
     {
         ui->lWidgetTareas->addItem(ui->lEditTarea->text());
+
+        calculatePerformedTasks();
     }
+}
+
+void MainWindow::calculatePerformedTasks()
+{
+    int numberItems = 0;
+    for (int i = 0; i < ui->lWidgetTareas->count(); ++i)
+    {
+        QListWidgetItem* item = ui->lWidgetTareas->item(i);
+        if (item->font().strikeOut())
+            numberItems++;
+    }
+
+    float valueToSet = (float)numberItems / (float)ui->lWidgetTareas->count();
+    ui->pBarProgress->setValue((int)(valueToSet * 100));
 }
 
 void MainWindow::on_push_pButtonDelete()
@@ -39,10 +85,23 @@ void MainWindow::on_push_pButtonDelete()
     if (ui->lWidgetTareas->selectedItems().size() != 0)
     {
         QListWidgetItem* itemToDelete = ui->lWidgetTareas->selectedItems().at(0);
+
+
+        lastDeletedIndex = ui->lWidgetTareas->row(itemToDelete);
+        lastDeletedText = itemToDelete->text().toStdString();
+        lastDeletedComplete = itemToDelete->font().strikeOut();
         ui->lWidgetTareas->takeItem(ui->lWidgetTareas->row(itemToDelete));
+
         ui->lWidgetTareas->removeItemWidget(itemToDelete);
 
         delete itemToDelete;
+
+        undone = false;
+
+        ui->actionRedo->setEnabled(false);
+        ui->actionUndo->setEnabled(true);
+
+        calculatePerformedTasks();
     }
 }
 
@@ -64,18 +123,11 @@ void MainWindow::on_push_pButtonComplete()
         }
 
         itemToChage->setFont(newFont);
+
+        calculatePerformedTasks();
     }
 
-    int numberItems = 0;
-    for (int i = 0; i < ui->lWidgetTareas->count(); ++i)
-    {
-        QListWidgetItem* item = ui->lWidgetTareas->item(i);
-        if (item->font().strikeOut())
-            numberItems++;
-    }
 
-    float valueToSet = (float)numberItems / (float)ui->lWidgetTareas->count();
-    ui->pBarProgress->setValue((int)(valueToSet * 100));
 }
 
 void MainWindow::on_push_pButtonEdit()
@@ -100,3 +152,56 @@ void MainWindow::on_triggered_Quit()
 
     quick_exit(0);
 }
+
+void MainWindow::on_triggered_Redo()
+{
+    //ui->lWidgetTareas->removeItemWidget(lastDeletedItem);
+    if (lastDeletedIndex != -1 && undone)
+    {
+        QListWidgetItem* itemToDelete = ui->lWidgetTareas->item(lastDeletedIndex);
+
+        ui->lWidgetTareas->takeItem(ui->lWidgetTareas->row(itemToDelete));
+
+        ui->lWidgetTareas->removeItemWidget(itemToDelete);
+
+        delete itemToDelete;
+
+        undone = false;
+
+        ui->actionRedo->setEnabled(false);
+        ui->actionUndo->setEnabled(true);
+
+        calculatePerformedTasks();
+    }
+}
+
+void MainWindow::on_triggered_Undo()
+{
+    if (lastDeletedIndex != -1 && !undone)
+    {
+        ui->lWidgetTareas->insertItem(lastDeletedIndex, QString::fromStdString(lastDeletedText));
+       // ui->lWidgetTareas->addItem(QString::fromStdString(lastDeletedText));
+        undone = true;
+
+        if (lastDeletedComplete)
+        {
+            QListWidgetItem* addedItem = ui->lWidgetTareas->item(lastDeletedIndex);
+            QFont newFont = addedItem->font();
+
+            newFont.setStrikeOut(true);
+
+            addedItem->setFont(newFont);
+        }
+
+        ui->actionRedo->setEnabled(true);
+        ui->actionUndo->setEnabled(false);
+
+        calculatePerformedTasks();
+
+    }
+
+
+
+
+}
+
--- Test01/mainwindow.h
+++ Test01/mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+// Los componentes se crean en un fichero que genera qt automáticamente que se llama ui_mainwindow.h
+
 #include <QMainWindow>
+#include <QKeyEvent>
+
+#include <QListWidgetItem>
 
 namespace Ui {
 class MainWindow;
@@ -16,15 +21,27 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_push_pButtonAdd();
+    void onItemAdded();
     void on_push_pButtonDelete();
     void on_push_pButtonComplete();
     void on_push_pButtonEdit();
 
     void on_triggered_Quit();
+    void on_triggered_Redo();
+    void on_triggered_Undo();
 
 private:
     Ui::MainWindow *ui;
+
+    int lastDeletedIndex = -1;
+    std::string lastDeletedText = "";
+    bool lastDeletedComplete = false;
+    bool undone = false;
+
+    void calculatePerformedTasks();
+
+protected:
+    void keyPressEvent(QKeyEvent* event);
 };
 
 #endif // MAINWINDOW_H
